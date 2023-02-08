895009edcd9e08e6717db8f4fc05d1f2f1fedb48
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-08 23:37:15 +0100

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,47 +1,47 @@
 <?php
 echo "TESST";
-// session_start();
-// function db_connect() {
-//     static $connection;
-// 	$username	= "ordremekka";   
-// 	$password	= "Gme1234567890987654321";
-// 	$dbname		= "gme";
-// 	$host		= "ordremekka.mysql.database.azure.com";
+session_start();
+function db_connect() {
+    static $connection;
+	$username	= "ordremekka";   
+	$password	= "Gme1234567890987654321";
+	$dbname		= "gme";
+	$host		= "ordremekka.mysql.database.azure.com";
     
     
 
-//     if(!isset($connection)) { 
-//         $connection = mysqli_init();
-//     mysqli_ssl_set($connection,NULL,NULL, "https://ordremekka.azurewebsites.net/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-//         mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
-//     }
-//     if($connection === false) {
-//         return mysqli_connect_error(); 
-//     }
-//     return $connection;
-// }
+    if(!isset($connection)) { 
+        $connection = mysqli_init();
 
-// function db_query($query) {
-//     $connection = db_connect();
-//     $result = mysqli_query($connection,$query);
-//     return $result;
-// }
+        mysqli_real_connect($connection, $host, $username, $password, $dbname, 3306, MYSQLI_CLIENT_SSL);
+    }
+    if($connection === false) {
+        return mysqli_connect_error(); 
+    }
+    return $connection;
+}
 
-// function db_error() {
-//     $connection = db_connect();
-//     return mysqli_error($connection);
-// }
+function db_query($query) {
+    $connection = db_connect();
+    $result = mysqli_query($connection,$query);
+    return $result;
+}
 
-// $connect = db_connect();
+function db_error() {
+    $connection = db_connect();
+    return mysqli_error($connection);
+}
 
-// if (!isset($_SESSION['token'])) {
-//     $token = md5(uniqid(rand(), TRUE));
-//     $_SESSION['token'] = $token;
-//     $_SESSION['token_time'] = time();
-// }else{
-//     $token = $_SESSION['token'];
-// }
+$connect = db_connect();
 
-// define('ADMINEMAIL', 'mads@garderobemekka.no');
+if (!isset($_SESSION['token'])) {
+    $token = md5(uniqid(rand(), TRUE));
+    $_SESSION['token'] = $token;
+    $_SESSION['token_time'] = time();
+}else{
+    $token = $_SESSION['token'];
+}
+
+define('ADMINEMAIL', 'mads@garderobemekka.no');
 
 ?>
