5ac429420022d7d995580ce1dc416f9942a0d458
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-09 01:26:00 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 echo "1";
 
 $con = mysqli_init();
-mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_ssl_set($con,NULL,NULL, "https://ordremekka.azurewebsites.net/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "ordremekka.mysql.database.azure.com", "ordremekka", "Gme1234567890987654321", "gme", 3306, MYSQLI_CLIENT_SSL);
   
   ?>
