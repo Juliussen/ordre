e4e7c42bb1f02fd72ee39ad4a2de67852f5fc4e6
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-09 02:21:58 +0100

    Update test.php

--- test.php
+++ test.php
@@ -2,7 +2,7 @@
 
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn,NULL,NULL, "/wwwroot/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, "ordremekka.mysql.database.azure.com", "ordremekka", "Gme1234567890987654321", "gme", 3306, MYSQLI_CLIENT_SSL);
   
   ?>
