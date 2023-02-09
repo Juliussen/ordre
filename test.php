2be940e277b5611b9559c4ca521164b5091b55ab
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-09 01:23:53 +0100

    Update test.php

--- test.php
+++ test.php
@@ -1,9 +1,8 @@
 <?php
 echo "1";
-$conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/assets/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-mysqli_real_connect($conn, 'ordremekka.mysql.database.azure.com', 'ordremekka', 'Gme1234567890987654321', 'gme', 3306, MYSQLI_CLIENT_SSL);
-if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "ordremekka.mysql.database.azure.com", "ordremekka", "Gme1234567890987654321", "gme", 3306, MYSQLI_CLIENT_SSL);
   
   ?>
