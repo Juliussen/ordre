f3792f17445a85ddad2a47d85d3a0dd96d28cee2
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-09 08:57:32 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -45,7 +45,7 @@ if($msg == 1){ $strmsg = "Invalid Username or Password"; }
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Login/ Registration Form</title>
+        <title>Ordremekka - 1.0</title>
         <link rel="stylesheet" href="assets/css/bootstrap.min.css">
         <?php if($darkmode == '1')
         {
