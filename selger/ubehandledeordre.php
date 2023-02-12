265f11d6043503d88b5904e17f8899e0aa29f721
Juliussen <124820520+Juliussen@users.noreply.github.com>
2023-02-12 21:39:25 +0100

    Update ubehandledeordre.php

--- selger/ubehandledeordre.php
+++ selger/ubehandledeordre.php
@@ -12,6 +12,7 @@ $sql = db_query("select * from google_auth where googlecode = '".$googlecode."'"
 $row = mysqli_fetch_array($sql);
 
 $firstname 	= $row['username'];
+$selgerepost 	= $row['selgerepost'];
 $lastname 	= $row['lname'];
 $email 		= $row['email'];
 $usertype 		= $row['usertype'];
@@ -268,7 +269,7 @@ $datosendtprod = $row['datosendtprod'];
                     <?php
                include_once('../conn.php');
 
-                $query = "SELECT * FROM ordre where (selger = '".$firstname."' and status = 'Ubehandlet') or (selger = '".$firstname."' and status = 'Forskudd Faktura Betalt') ORDER BY `datoopprettet` ASC";
+                $query = "SELECT * FROM ordre where (selger = '".$firstname."' and status = 'Ubehandlet') or (selger = '".$firstname."' and status = 'Forskudd Faktura Betalt') or (selgerepost = '".$selgerepost."' and status = 'Ubehandlet') ORDER BY `datoopprettet` ASC";
                 $query_run = mysqli_query($connection, $query);
                 $ordrenr = $row["ordrenr"];
                 $ordrenr1 = $row["ordrenr1"];
